1dbf261ef036c96794948a6fff42c04fa5a6042d
NAGARAJU <goudpnagaraju@gmail.com>
2020-03-08 00:04:44 +0530

    added example class for master

---
+++ src/info/firstPackage/TestExampleMaster.java
@@ -0,0 +1,11 @@
+package info.firstPackage;
+
+public class TestExampleMaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("inside master example class");
+	}
+
+}
